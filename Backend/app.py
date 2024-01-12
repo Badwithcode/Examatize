655fb96d5a3e038bd026cb8e183ad9d352dd8037
MrMohamedRizwan <109430288+MrMohamedRizwan@users.noreply.github.com>
2024-01-12 12:20:01 +0530

    Login API Constructor

--- Backend/app.py
+++ Backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
 from Sources.Users import UsersResource
+from Sources.Login import LoginResource
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +10,7 @@ api = Api(app)
 
 
 api.add_resource(UsersResource, "/users")
+api.add_resource(LoginResource, "/login")
 
 
 @app.get("/test")
