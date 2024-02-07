7afd2237a82c2e37c7c03e689b78bcd893a009d2
naveen-ravi2810 <rnaveen28102003@gmial.com>
2024-02-07 23:15:08 +0530

    Frontend deleted

--- Frontend/src/pages/Index.jsx
+++
@@ -1,4 +0,0 @@
-export * from './PreviousTest'
-export * from './UpcomingTest'
-export * from './Profile'
-export * from './Dashboard'
--- Frontend/src/pages/UpcomingTest.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UpcomingTest = () => {
-  return (
-    <div>UpcomingTest</div>
-  )
-}
-
-export {UpcomingTest}
