eca8fdd35e9b06f2d5b09cece4956a3b558515f7
MrMohamedRizwan <109430288+MrMohamedRizwan@users.noreply.github.com>
2024-02-09 09:00:53 +0530

    deleted

--- Backend/Schemas/StudentSchema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class UserAddBase(BaseModel):
-    user_name: str
-    password: str
-    email: str
-    phone: int = Field()
-    role: str
--- Backend/Sources/__init__.py
+++
@@ -1,5 +0,0 @@
-from Sources.Users import UsersResource
-from Sources.Login import LoginResource, LogoutResource
-from Sources.Signup import StudentCreateResource
-from Sources.Token import TokenResource
-from Sources.Password import PasswordResource
