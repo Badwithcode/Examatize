ad2808b590e9f2aa8c04673c87af152ce95844df
MrMohamedRizwan <109430288+MrMohamedRizwan@users.noreply.github.com>
2024-02-09 08:57:06 +0530

    nkjng

--- frontend/src/pages/jubair.js
+++ frontend/src/pages/jubair.js
@@ -6,6 +6,8 @@ const jubair = () => {
       <div className={'text-[100px] border-white border-2 w-[] h-[] flex flex-col items-center justify-center'+""}>
         <h1>Hello Jubair</h1>
         <h1>Hello Jubair</h1>
+        <h1>Hello Jubair</h1>
+
 
 
       </div>
